cc43e20791d4aec994e63eb2a6f8226da0c44d8a
iToday <xba1987@126.com>
2019-08-31 20:58:48 +0800

    【功能】增加常用命令

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # LoggerParser
 日志分析工具
+
+代码push:
+git push lab HEAD:master
--- learn_01.py
+++ learn_01.py
@@ -1,10 +1,21 @@
 import sys
 
-from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtWidgets import QWidget, QApplication, QTextBrowser
+from PyQt5 import QtCore
+
+from ClassDemo import ClassDemo
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     q = QWidget()
+    
+    q.textBrowser = QTextBrowser(q)
+    q.textBrowser.setGeometry(QtCore.QRect(20, 100, 351, 191))
+    q.textBrowser.setObjectName("textBrowser")
+    cls = ClassDemo("fuck")
+
+    q.textBrowser.setText(cls.name)
+    
     q.show()
     sys.exit(app.exec_())
 
